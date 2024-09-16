5fd4d9e880396bc52895b847e03c4a4153141de7
Sohail <sohail@users.noreply.github.com>
2024-09-16 19:36:03 +0500

    Update main.go, the main function now calls the newly added argsvGoHelp functionHelp

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 /*
- * main.go
+	 main.go
+	 Q@khaa.pk
  */
 
 package main
 
-import "fmt"
+import (
+		 "fmt"
+		 "os"
+)
 
 var command = "?,/?,h,-h,/h,help,-help,--help(Displays help screen)#v,-v,/v,version,-version,--version(Displays version number)#d,-d,/d,dir,--dir(List files)"
 
@@ -16,10 +20,21 @@ func main() {
 		fmt.Println("Command found, number of instances = ", ll.length)
 		node := ll.head
 		for i := uint32(0); i < ll.length; i++ {
-			fmt.Println(node.c, " ", node.i, " ", node.n, " ", node.o)
-			node = node.next
+			fmt.Println( "Index into command string = ", node.c, 
+						 "\nIndex into argv = ", node.i, "\nargc of this command = ", node.n, "\nIndex into the command string for one praticulr name used = ", node.o, "\n and it is \"", os.Args[node.i] , "\"")
+			
+			fmt.Println("The command and its arguments are...") 			 
+			for j := uint32(0); j < node.n; j++ {
+				fmt.Println(os.Args[node.i + j])
+			}
+
+			fmt.Println("Help String = " + argsvGoHelp(node.c) + "\n")
+			node = node.next			
 		}
 	}
 
-	fmt.Println("--> ", ll.commonArgs)
+	fmt.Println("\nNumber of common arguments = ", ll.commonArgs)
+	for i := uint32(0); i < ll.commonArgs; i++ {
+		fmt.Println(os.Args[i])
+	}
 }
