fcfebf68f7beb0031b7c8991ed6f38ceef56f374
Sohail <sohail@users.noreply.github.com>
2024-09-17 17:58:46 +0500

    Update main.go

--- main.go
+++ main.go
@@ -10,6 +10,21 @@ import (
 		 "os"
 )
 
+/*
+	Each command group is separated by #,
+	and within each group, 
+	different aliases for the same command are separated by commas.
+	The text in parentheses provides help information for each command.
+
+	?, /h, -h, --help: These are aliases for the "help" command.
+	v, -v, version: Aliases for version command.
+	d, -d, dir: These are for directory listing.
+	
+	Note:
+	The delimiters used for parsing commands and arguments in this file can be customized.
+	To modify the delimiters (e.g., for command argument parsing, command separation, or help strings),
+	please refer to the `constants.go` file where these tokens are defined.
+ */
 var command = "?,/?,h,-h,/h,help,-help,--help(Displays help screen)#v,-v,/v,version,-version,--version(Displays version number)#d,-d,/d,dir,--dir(List files)"
 
 func main() {
