7d445b2a3913f53dd5077f998a9acb2f3914c4e5
Sohail <sohail@users.noreply.github.com>
2024-09-17 19:30:27 +0500

    Update constants.go

--- constants.go
+++ constants.go
@@ -5,6 +5,17 @@
 
 package main
 
+/*
+  This file defines the tokens used for parsing commands and their arguments.
+  You can modify these constants to change the delimiters for:
+  - Command argument parsing (currently '#')
+  - Command separation (currently ',')
+  - Help string start (currently '(')
+  - Help string end (currently ')')
+
+  Modify these as needed to suit your application's requirements.
+*/
+
 const commandArgumentParserToken rune = '#'
 const commandParserToken rune = ','
 const commandHelpStringParserTokenEnd rune = ')'
